995ea946e520ae9474128df65f76efb1ab78d3bc
Yilei Bai <marbink100@gmail.com>
2025-02-12 17:09:56 -0500

    implemented coroutines into how enemies spawn. Checks for max enemy count

--- Assets/FPSGameAssets/Scripts/EnemySpawner.cs
+++ Assets/FPSGameAssets/Scripts/EnemySpawner.cs
@@ -1,11 +1,12 @@
+using System.Collections;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
     //this will be a reference to the Dementor pre-fab in the Assets folder, NOT the hierarchy
     public GameObject enemyPrefab;
-
     public int spawnFrequency = 5;
+    public int maxEnemyCount = 5;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,7 +14,9 @@ public class EnemySpawner : MonoBehaviour
         if (!enemyPrefab)
             return;
         
-        InvokeRepeating("SpawnEnemy", 2, spawnFrequency);
+        // InvokeRepeating("SpawnEnemy", 2, spawnFrequency);
+        Debug.Log("Coroutine started --------- " + Time.time);
+        StartCoroutine(SpawnEnemies(spawnFrequency));
     }
 
     void SpawnEnemy()
@@ -21,5 +24,23 @@ public class EnemySpawner : MonoBehaviour
         var positionOffset = Random.insideUnitSphere * 20;
 
         Instantiate(enemyPrefab, transform.position + positionOffset, transform.rotation);
+    }
+
+    IEnumerator SpawnEnemies(float spawnInterval)
+    {
+        Debug.Log("Before yield:" + Time.time);
+
+        while (true) {
+
+            int enemyCount = GameObject.FindGameObjectsWithTag("Dementor").Length;
+
+            //check to see if we've reached the enemy count limit
+            if (enemyCount < maxEnemyCount) {
+                SpawnEnemy();
+            }
+            
+            yield return new WaitForSeconds(spawnInterval);
+            Debug.Log("After yield: " + Time.time);
+        }
     }
 }
