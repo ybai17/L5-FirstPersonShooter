3170a6a99a5f11a197dafb9208f75c4b183d1479
Yilei Bai <marbink100@gmail.com>
2025-02-18 14:54:10 -0500

    implemented breakable crate mechanic

--- Assets/FPSGameAssets/Scripts/Breakable.cs
+++ Assets/FPSGameAssets/Scripts/Breakable.cs
@@ -1,8 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Breakable : MonoBehaviour
 {
-   
+   public GameObject cratePieces;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Instantiate(cratePieces, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
 }
